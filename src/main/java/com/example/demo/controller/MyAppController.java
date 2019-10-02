323feb89ef48ce3f6ddc2150ce42b77cb1663583
Amit Kumar <amit.kumar@sapientrazorfish.com>
2019-10-01 21:55:58 -0400

    My First Comment

---
+++ src/main/java/com/example/demo/controller/MyAppController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyAppController {
+
+	@RequestMapping("/welcome")
+	@ResponseBody
+	public String handler() {
+		return "Hello World!!!";
+	}
+	
+	@RequestMapping("/")
+	public String handlerHtml() {
+		return "myapp.html";
+	}
+}
